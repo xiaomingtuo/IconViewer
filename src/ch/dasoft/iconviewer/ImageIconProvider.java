996eb98b6be7e94d2ca6a6691c24cbdffe95d5a5
xiaoming.tuo <xiaoming.tuo@msxf.com>
2018-08-03 17:04:26 +0800

    cache the image

--- src/ch/dasoft/iconviewer/ImageIconProvider.java
+++ src/ch/dasoft/iconviewer/ImageIconProvider.java
@@ -3,20 +3,25 @@ package ch.dasoft.iconviewer;
 import com.intellij.ide.IconProvider;
 import com.intellij.psi.PsiElement;
 import com.intellij.psi.PsiFile;
+import com.twelvemonkeys.util.LRUHashMap;
 import org.jetbrains.annotations.NotNull;
 
 import javax.swing.*;
 import java.awt.*;
+import java.util.HashMap;
 
 
 /**
  * Created by David Sommer on 19.05.17.
+ *
  * @author davidsommer
  */
 public class ImageIconProvider extends IconProvider {
 
     private static final int IMG_WIDTH = 16;
     private static final int IMG_HEIGHT = 16;
+    private HashMap<String, Image> cacheImage = new LRUHashMap<>();
+    private HashMap<Image, ImageIcon> cacheIcon = new LRUHashMap<>();
 
     public Icon getIcon(@NotNull PsiElement psiElement, int flags) {
         PsiFile containingFile = psiElement.getContainingFile();
@@ -24,14 +29,27 @@ public class ImageIconProvider extends IconProvider {
             Image image;
             try {
 //                image = ImageLoader.loadFromStream(new BufferedInputStream(new FileInputStream(containingFile.getVirtualFile().getCanonicalFile().getCanonicalPath())));
-                image = Toolkit.getDefaultToolkit().getImage(containingFile.getVirtualFile().getCanonicalFile().getCanonicalPath());
+                String path = containingFile.getVirtualFile().getCanonicalFile().getCanonicalPath();
+                if (cacheImage.get(path) != null) {
+                    image = cacheImage.get(path);
+                } else {
+                    image = Toolkit.getDefaultToolkit().getImage(path);
+                    cacheImage.put(path, image);
+                }
             } catch (Exception e) {
                 e.printStackTrace();
                 throw new IllegalStateException("Error loading preview Icon - " + containingFile.getVirtualFile().getCanonicalFile().getCanonicalPath());
             }
 
             if (image != null) {
-                return new ImageIcon(image.getScaledInstance(IMG_WIDTH, IMG_HEIGHT, Image.SCALE_SMOOTH));
+                ImageIcon icon;
+                if (cacheIcon.get(image) != null) {
+                    icon = cacheIcon.get(image);
+                } else {
+                    icon = new ImageIcon(image.getScaledInstance(IMG_WIDTH, IMG_HEIGHT, Image.SCALE_SMOOTH));
+                    cacheIcon.put(image, icon);
+                }
+                return icon;
             }
         }
         return null;
